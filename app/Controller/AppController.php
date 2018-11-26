8121dc786f3e3c8216a67c243fd982675ba417e1
khilarymilan <khilarymilan@gmail.com>
2018-11-27 06:39:36 +0800

    add sql dump

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -27,12 +27,12 @@ App::uses('Controller', 'Controller');
  * Add your application-wide methods in the class below, your controllers
  * will inherit them.
  *
- * @package		app.Controller
- * @link		https://book.cakephp.org/2.0/en/controllers.html#the-app-controller
+ * @package     app.Controller
+ * @link        https://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
 
-	public $components = [
+    public $components = [
         'Flash',
         'Auth' => [
             'loginRedirect' => [
